9a4288ec7450cec9fffcf945faca9a36ae1e1f04
Tiroleza <fefetiroleza@gmail.com>
2023-12-19 13:43:34 -0300

    initial commit

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) {
+app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
 });
 
